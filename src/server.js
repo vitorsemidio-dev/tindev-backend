4db578626cc69829bbdf0c46af2fcd17ab253df4
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-08-07 22:33:42 -0300

    mongoose, conexao com bd e arquivo de autenticacao

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
+const authenticate = require('./config/authenticate.json');
 
 const server = express();
 const PORT = 3000;
+
+const { user } = authenticate;
+const { password } = authenticate;
+const url = `mongodb+srv://${user}:${password}@cluster0-uw7wb.mongodb.net/omnistack8?retryWrites=true&w=majority`;
+
+mongoose.connect( url, { useNewUrlParser: true } );
 
 server.use(express.json());
 server.use(routes);
