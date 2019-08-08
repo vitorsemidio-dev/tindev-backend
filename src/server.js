ea280b0875cbf3c76653e1265d6f7602197712ad
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-08-07 22:04:41 -0300

    Nodemon instalado

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const server = express();
 const PORT = 3000;
 
 server.get('/', (req, res) => {
-    return res.send(`Hello ${req.query.name}. Bem vindo ao Tinder`);
+    return res.send(`Hello ${req.query.name}. Bem vindo ao Tinder.`);
 })
 
 server.listen(PORT);
