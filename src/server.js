97b503a9f48637ab8695876f014278d9f6cf6f7e
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-08-11 09:03:09 -0300

    config socket.io

--- src/server.js
+++ src/server.js
@@ -2,19 +2,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
-const authenticate = require('./config/authenticate.json');
 
-const server = express();
+
+const authenticate = require('./config/authenticate.json');
 const PORT = 3333;
+
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+io.on('connection', socket => {
+    console.log('Nova conexão', socket.id);
+});
+
 
 const { user } = authenticate;
 const { password } = authenticate;
 const url = `mongodb+srv://${user}:${password}@cluster0-uw7wb.mongodb.net/omnistack8?retryWrites=true&w=majority`;
-
 mongoose.connect( url, { useNewUrlParser: true } );
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
 
 server.listen(PORT);
